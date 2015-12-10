538f6052d1e3e3bb76bc377eb83971416169c405
Sky Su <februaryskys@gmail.com>
2015-12-10 10:16:14 +0000

    Adds edit and delete functionality to Match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -14,6 +14,21 @@ class MatchesController < ApplicationController
     redirect_to matches_path
   end
 
+  def edit
+    @match = Match.find(params[:id])
+    @teams = Team.all
+  end
+
+  def update
+    Match.find(params[:id]).update(match_params)
+    redirect_to matches_path
+  end
+
+  def destroy
+    Match.find(params[:id]).destroy
+    redirect_to matches_path
+  end
+
   private
   def match_params
     params.require(:match).permit(:competition, :team_id, :home_team_id,  :team_score, :home_team_score)
